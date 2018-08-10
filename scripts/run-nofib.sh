fb8236b3f00bd7c27e5b765c90916d2425188891
Sebastian Graf <sebastian.graf@kit.edu>
2018-08-10 18:47:17 +0200

    Experimental changes to the nofib run script

--- scripts/run-nofib.sh
+++ scripts/run-nofib.sh
@@ -1,32 +1,16 @@
+#! /usr/bin/env bash
 test -d nofib || { echo "No nofib found" ; exit 1 ;  }
 
 set -e
 set -o pipefail
 
 mode=norm
-variant=""
-cachegrind=
-cachegrindconf=
-NoFibRuns=20
+variant="-$mode"
 clean=yes
 threads=$(($(nproc --all) + 1))
-
-
-# defaults for the thesis
-# full script at https://github.com/nomeata/ghc-devscripts
-
-mode=slow
-variant="$variant-slow"
-
 cachegrindconf=--disable-large-address-space
-cachegrind=EXTRA_RUNTEST_OPTS=-cachegrind
-NoFibRuns=1
-variant="$variant-cachegrind"
 
-echo mode: $mode
 echo variant: $variant
-
-#git fetch origin
 
 name="$(date +'%Y-%m-%d-%H-%M')"
 
@@ -35,13 +19,12 @@ diff="$(basename $PWD)"
 echo name: $name
 
 echo ready to go...
-#sleep 10
 
 if [ "$clean" = yes ]
 then
 	make distclean
 	echo "BuildFlavour = bench" | cat - mk/build.mk.sample > mk/build.mk
-    perl boot
+	perl boot
 	./configure $cachegrindconf
 	/usr/bin/time -o buildtime-$name make -j$threads 2>&1 |
 		tee /logs/buildlog-$diff-$name.log
@@ -51,6 +34,6 @@ fi
 cd nofib/
 make clean
 make boot
-(make EXTRA_RUNTEST_OPTS='-cachegrind +RTS -V0 -RTS' -j$threads NoFibRuns=1) 2>&1 | tee /logs/$diff-$name.log
+(make EXTRA_RUNTEST_OPTS='-cachegrind +RTS -V0 -RTS' EXTRA_HC_OPTS='-fllvm -optlo -Os' mode=$mode -j$threads NoFibRuns=1) 2>&1 | tee /logs/$diff-$name.log
 # fix a problem with nofib logs from cachegrind
 sed -i -e 's/,  L2 cache misses/, 0 L2 cache misses/' /logs/$diff-$name.log
